d89f996b6c7f1a304710683d333142f8a70a41fa
Mark Piper <mark.piper@colorado.edu>
2021-12-01 16:09:43 -0700

    Update tests for control, info, variable, and grid functions

--- src/test/java/edu/colorado/csdms/bmiheat/TestVarMethods.java
+++ src/test/java/edu/colorado/csdms/bmiheat/TestVarMethods.java
@@ -3,32 +3,45 @@ package edu.colorado.csdms.bmiheat;
 import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * JUnit tests for the getter and setter methods of the {@link BmiHeat} class.
  */
-@Ignore
 public class TestVarMethods {
 
   private static final int SIZEOF_DOUBLE = 8;
 
+  private int gridId;
   private String varName;
   private String varUnits;
   private String varType;
+  private String varLocation;
   private Integer gridSize;
 
   @Before
   public void setUp() throws Exception {
+    gridId = 0;
     varName = "plate_surface__temperature";
     varUnits = "K";
     varType = "double";
+    varLocation = "node";
     gridSize = 48;
   }
 
   @After
   public void tearDown() throws Exception {
+  }
+
+  /**
+   * Test method for {@link BmiHeat#getVarGrid(java.lang.String)}.
+   */
+  @Test
+  public final void testGetVarGrid() {
+    BmiHeat component = new BmiHeat();
+    component.initialize();
+
+    assertEquals(gridId, component.getVarGrid(varName));
   }
 
   /**
@@ -38,6 +51,7 @@ public class TestVarMethods {
   public final void testGetVarType() {
     BmiHeat component = new BmiHeat();
     component.initialize();
+
     assertEquals(varType, component.getVarType(varName));
   }
 
@@ -48,6 +62,7 @@ public class TestVarMethods {
   public final void testGetVarUnits() {
     BmiHeat component = new BmiHeat();
     component.initialize();
+
     assertEquals(varUnits, component.getVarUnits(varName));
   }
 
@@ -58,6 +73,7 @@ public class TestVarMethods {
   public final void testGetVarItemsize() {
     BmiHeat component = new BmiHeat();
     component.initialize();
+
     assertEquals(SIZEOF_DOUBLE, component.getVarItemsize(varName));
   }
 
@@ -68,8 +84,19 @@ public class TestVarMethods {
   public final void testGetVarNbytes() {
     BmiHeat component = new BmiHeat();
     component.initialize();
+
     double[] varCpy = new double[gridSize];
     component.getValue(varName, varCpy);
+
     assertEquals(SIZEOF_DOUBLE * varCpy.length, component.getVarNbytes(varName));
   }
+
+  @Test
+  public final void testGetVarLocation() {
+    BmiHeat component = new BmiHeat();
+    component.initialize();
+
+    assertEquals(varLocation, component.getVarLocation(varName));
+  }
+
 }
